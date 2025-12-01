8e4840ccb5573f455197a1123bc7c184dd5d0cc8
mkonig <koenig@v-i-t.de>
2025-12-01 07:58:57 +0100

    chore: Remove env script from shell.nix

--- shell.nix
+++ shell.nix
@@ -13,28 +13,11 @@ let
     gum
     cocogitto
   ];
-
-  packageVersions = pkgs.lib.concatMapStringsSep "\n"
-    (package:
-      "echo \"${package.pname or package.name}|${package.version}\""
-    )
-    packages;
-
-  show-env = pkgs.writeShellScriptBin "show-env" ''
-    header=$(gum format -t template '{{ Background "250" (Bold "ork-o-scope.nvim") }}{{ Background "250" " development environment:" }}')
-    table=$(
-      {
-        ${packageVersions}
-      } | gum table --separator "|" --columns "Package,Version" --print
-    )
-    gum join --vertical --align="left" "$header" "$table"
-  '';
 in
 pkgs.mkShell {
-  buildInputs = packages ++ [ show-env ];
+  buildInputs = packages;
 
   shellHook = ''
-    show-env
     echo "Run 'show-env' to display this information again"
   '';
 }
